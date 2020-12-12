aa6467a01ae45d06ebc241b2b4c380fdc812e565
Axel ESSONO <naxelcdric@gmail>
2020-12-12 04:03:22 +0100

    Added category list

--- app/src/main/java/com/obiangetfils/akiba/my_data/SourceData.kt
+++ app/src/main/java/com/obiangetfils/akiba/my_data/SourceData.kt
@@ -1,7 +1,29 @@
 package com.obiangetfils.akiba.my_data
 
+import com.obiangetfils.akiba.R
+
 abstract class SourceData {
 
+    companion object {
 
+        /** SliderView **/
+        val slideMap: MutableMap<String, Int> = mutableMapOf(
+            "Plombier" to R.drawable.ic_plombier,
+            "Menuisier" to R.drawable.ic_menuisier,
+            "Maçon" to R.drawable.ic_macon
+        )
+
+        /** Categorie **/
+        val categoryMap: MutableMap<String, Int> = mutableMapOf(
+            "Plombier" to R.drawable.ic_plombier,
+            "Menuisier" to R.drawable.ic_menuisier,
+            "Maçon" to R.drawable.ic_macon,
+            "Chauffeur" to R.drawable.ic_chauffeur,
+            "Coiffeur" to R.drawable.ic_coiffeur,
+            "Charpentier" to R.drawable.ic_charpentier,
+            "Femme de menage" to  R.drawable.ic_femme_menage
+        )
+
+    }
 
 }
