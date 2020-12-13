04e2264f8fd8269a54b3c3fe26e1dbb39eacba6b
Axel ESSONO <naxelcdric@gmail>
2020-12-13 04:12:46 +0100

    Added Category and HandyMan models

---
+++ app/src/main/java/com/obiangetfils/akiba/model/Category.kt
@@ -0,0 +1,3 @@
+package com.obiangetfils.akiba.model
+
+data class Category(var categoryName:String, var categoryImage: Int)
---
+++ app/src/main/java/com/obiangetfils/akiba/model/HandyMan.kt
@@ -0,0 +1,12 @@
+package com.obiangetfils.akiba.model
+
+data class HandyMan(
+    var handyManId: Int,
+    var handyManName: String,
+    var handyManFirstName: String,
+    var handyManServicePrice: Int,
+    var handyManCategory: String,
+    var handyManImage:Int,
+    var handyManDescription:String,
+    var handyManNote: Float,
+)
